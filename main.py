b93bd337901026c18c3d32e693952ba3c6f31ef9
killrpenguin <dmcfarland8031@gmail.com>
2023-07-24 21:04:08 -0600

    Updated ddb function to clean data. Fixed depreciation error in selenium webdriver

--- main.py
+++ main.py
@@ -1,37 +1,62 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import requests
+import random
 
+
+def ddb_list() -> list:
+    url = "https://cedh-decklist-database.com/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    moxfield = []
+    archideckt = []
+    tappedout = []
+    link_list = []
+    project_href = [i['href'] for i in soup.find_all('a', href=True)]
+    for i in project_href:
+        mxfld = re.search("moxfield", i)
+        arch = re.search("archidekt", i)
+        tppd = re.search("tappedout", i)
+        if mxfld != None:
+            moxfield.append(mxfld.string)
+        if arch != None:
+            archideckt.append(arch.string)
+        if tppd != None:
+            tappedout.append(tppd.string)
+
+    for a in moxfield:
+        link_list.append(a)
+    for b in archideckt:
+        link_list.append(b)
+    for c in tappedout:
+        link_list.append(c)
+    """    f = open("list", "w")
+    for d in link_list:
+        f.write(d + "\n")
+    f.close()"""
+    return link_list
+
+
+def get_moxfield_lists():
 # Use headless browser to scrape deck list
-options = Options()
-options.headless = True
-f = open("Heliod.txt", "a")
-driver = webdriver.Chrome('C:\chromedriver', options=options)
+    pause = random.randint(5, 15)
+    options = Options()
+    options.add_argument("--headless")
+    s = Service('C:\chromedriver\chromedriver.exe')
+    driver = webdriver.Chrome(service=s, options=options)
 
-driver.get('https://www.moxfield.com/decks/urs4pKTeCUyZ-FJimN5prA')
-driver.implicitly_wait(10)
+    driver.get('https://www.moxfield.com/decks/urs4pKTeCUyZ-FJimN5prA')
+    driver.implicitly_wait(pause)
 
-decklist = driver.find_elements(By.CLASS_NAME, "table-deck-row-link.text-body")
-deckname = driver.find_elements(By.CLASS_NAME, "deck-header-name")
-for i in decklist:
-    print(i.text)
-    f.write(i.text + '\n')
+    decklist = driver.find_elements(By.CLASS_NAME, "table-deck-row-link.text-body")
 
+    for i in decklist:
+        print(i.text)
 
-driver.close()
+    driver.close()
 
-# Gather list from DDB
-url = "https://cedh-decklist-database.com/"
-page = requests.get(url)
-f = open("list.txt", "a")
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-project_href = [i['href'] for i in soup.find_all('a', href=True)]
-for i in project_href:
-    print(i + "\n")
-    f.write(i + "\n")
-
-f.close()
+get_moxfield_lists()
