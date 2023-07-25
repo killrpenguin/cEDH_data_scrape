522aba7f81b03a807031259696500366a833918d
killrpenguin <dmcfarland8031@gmail.com>
2023-07-25 00:39:54 -0600

    Updated ddb function to clean data. Fixed depreciation error in selenium webdriver

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import requests
 import random
+import re
 
 
 def ddb_list() -> list:
     url = "https://cedh-decklist-database.com/"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    moxfield = []
-    archideckt = []
-    tappedout = []
-    link_list = []
+    moxfield, archideckt, tappedout, link_list = [], [], [], []
     project_href = [i['href'] for i in soup.find_all('a', href=True)]
     for i in project_href:
         mxfld = re.search("moxfield", i)
@@ -59,4 +56,25 @@ def get_moxfield_lists():
 
     driver.close()
 
-get_moxfield_lists()
+
+def get_tappedout_lists() -> list:
+    ret_list = []
+    pause = random.randint(5, 15)
+    options = Options()
+    options.add_argument("--headless")
+    s = Service('C:\chromedriver\chromedriver.exe')
+    driver = webdriver.Chrome(service=s, options=options)
+
+    driver.get('https://tappedout.net/mtg-decks/druid-breach/')
+    driver.implicitly_wait(pause)
+
+    decklist = driver.find_elements(By.CLASS_NAME, "card-link.card-hover")
+
+    for i in decklist:
+        print(i.text)
+        ret_list.append(i.text)
+    driver.close()
+    return ret_list
+
+
+get_tappedout_lists()
