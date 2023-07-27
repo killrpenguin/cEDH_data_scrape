30113a3d71cf3932dd45f27f37eb27dde3b1f228
killrpenguin <dmcfarland8031@gmail.com>
2023-07-27 15:30:42 -0600

    completed tappedout function added returns to both get decklist functions.

--- main.py
+++ main.py
@@ -3,74 +3,132 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
+from pathlib import Path
+import pandas as pd
 import requests
 import random
 import re
 
 
-def ddb_list() -> list:
+def ddb_list() -> tuple:
     url = "https://cedh-decklist-database.com/"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    moxfield, archideckt, tappedout, link_list = [], [], [], []
-    project_href = [i['href'] for i in soup.find_all('a', href=True)]
-    for i in project_href:
-        mxfld = re.search("moxfield", i)
-        arch = re.search("archidekt", i)
-        tppd = re.search("tappedout", i)
+    moxfield, tappedout = [], []
+    project_href = [spi['href'] for spi in soup.find_all('a', href=True)]
+    for phref in project_href:
+        mxfld = re.search("moxfield", phref)
+        tppd = re.search("tappedout", phref)
         if mxfld != None:
             moxfield.append(mxfld.string)
-        if arch != None:
-            archideckt.append(arch.string)
         if tppd != None:
             tappedout.append(tppd.string)
 
-    for a in moxfield:
-        link_list.append(a)
-    for b in archideckt:
-        link_list.append(b)
-    for c in tappedout:
-        link_list.append(c)
-    """    f = open("list", "w")
-    for d in link_list:
-        f.write(d + "\n")
-    f.close()"""
-    return link_list
+    return moxfield, tappedout
 
 
-def get_moxfield_lists() -> list:
+def get_moxfield_lists(proxy, deck_address) -> list:
     ret_list = []
     pause = random.randint(5, 15)
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--proxy_server=%s" % proxy)
     s = Service('C:\chromedriver\chromedriver.exe')
 
     driver = webdriver.Chrome(service=s, options=options)
-    driver.get('https://www.moxfield.com/decks/urs4pKTeCUyZ-FJimN5prA')
+    driver.get(deck_address)
     driver.implicitly_wait(pause)
     decklist = driver.find_elements(By.CLASS_NAME, "table-deck-row-link.text-body")
 
-    for i in decklist:
-        ret_list.append(i.text)
+    for imxrt in decklist:
+        ret_list.append(imxrt.text)
     driver.close()
 
     return ret_list
 
 
-def get_tappedout_lists() -> list:
-    ret_list = []
+def get_tappedout_lists(proxy, deck_address1) -> list:
+    ret_list1 = []
     pause = random.randint(5, 15)
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--proxy_server=%s" % proxy)
     s = Service('C:\chromedriver\chromedriver.exe')
 
     driver = webdriver.Chrome(service=s, options=options)
-    driver.get('https://tappedout.net/mtg-decks/druid-breach/')
+    driver.get(deck_address1)
     driver.implicitly_wait(pause)
     decklist = driver.find_elements(By.CLASS_NAME, "card-link.card-hover")
 
-    for i in decklist:
-        print(i.text)
-        ret_list.append(i.text)
+    for itpd in decklist:
+        ret_list1.append(itpd.text)
     driver.close()
-    return ret_list
+    return ret_list1
+
+
+def log_scrape(loglink, qty):
+    f = open("scrape_log.txt", "a")
+    f.write("scraped " + str(qty) + " from: " + str(loglink) + "\n")
+    f.close()
+
+
+def backup_work(scrapped_list):
+    df1 = pd.read_pickle(r"C:\Users\dmcfa\Desktop\cedh_webscrape1.pk1")
+    # df1 = pd.DataFrame(scrapped_list, columns=['Card_names'])
+    df2 = pd.DataFrame(scrapped_list, columns=['Card_names'])
+    df_to_save = pd.concat([df1, df2], axis=0)
+    df_to_save.to_pickle(r"C:\Users\dmcfa\Desktop\cedh_webscrape1.pk1")
+    print(df_to_save.tail(4))
+    print(str(df_to_save.shape[0]) + ' Rows in Pickle file.')
+
+def scraper():
+    proxies_list = open("tested_proxies", "r").read().strip().split("\n")
+    while_loop_cntrl, valid_proxy, mx_deck_address, tp_deck_address = 0, "", "", ""
+    VALID_STATUSES = [200, 301, 302, 307, 404]
+    moxfld, tppdout = ddb_list()
+    print("Moxfield decklists: " + str(len(moxfld)) + " TappedOut decklists: " + str(len(tppdout)))
+    while while_loop_cntrl <= 10000:
+        # Check the proxy is clean
+        for proxy in proxies_list:
+            try:
+                response = requests.get(url="http://ident.me/", proxies={'http': f"http://{proxy}"}, timeout=10)
+                if response.status_code in VALID_STATUSES:
+                    valid_proxy = proxy
+                    proxies_list.remove(proxy)
+                    break
+            except Exception as e:
+                print("Exception: ", type(e))
+        if len(proxies_list) == 0:
+            proxies_list = open("tested_proxies", "r").read().strip().split("\n")
+        if len(moxfld) > 0:
+            for mx_deck_addr in moxfld:
+                mx_deck_address = mx_deck_addr
+                moxfld.remove(mx_deck_addr)
+                print('Moxfield Decks left: ' + str(len(moxfld)))
+                break
+            mx_deck_list = get_moxfield_lists(valid_proxy, mx_deck_address)
+            mx_deck_list = [a for a in mx_deck_list if a]
+            backup_work(mx_deck_list)
+            print(mx_deck_address)
+            log_scrape(mx_deck_address, len(mx_deck_list))
+        while_loop_cntrl += 1
+        print('Decks scraped: ' + str(while_loop_cntrl))
+        if len(tppdout) > 0:
+            for tp_deck_addr in tppdout:
+                tp_deck_address = tp_deck_addr
+                tppdout.remove(tp_deck_addr)
+                print('TappedOut Decks left: ' + str(len(tppdout)))
+                break
+            tp_deck_list = get_tappedout_lists(valid_proxy, tp_deck_address)
+            tp_deck_list = [b for b in tp_deck_list if b]
+            backup_work(tp_deck_list)
+            print(tp_deck_address)
+            log_scrape(tp_deck_address, len(tp_deck_list))
+        if len(moxfld) == 0 and len(tppdout) == 0:
+           break
+
+        while_loop_cntrl += 1
+        print('Decks scraped: ' + str(while_loop_cntrl))
+
+
+scraper()
---
+++ proxy_script.py
@@ -0,0 +1,49 @@
+import requests
+
+
+proxies_list = open("http.txt", "r").read().strip().split("\n")
+unchecked_set = set(proxies_list)
+working_set, broken_set = set(), set()
+VALID_STATUSES = [200, 301, 302, 307, 404]
+
+
+def reset_proxy(proxy):
+    unchecked_set.add(proxy)
+    working_set.discard(proxy)
+    broken_set.discard(proxy)
+
+
+def set_working(proxy):
+    unchecked_set.discard(proxy)
+    working_set.add(proxy)
+    broken_set.discard(proxy)
+
+
+def set_not_working(proxy):
+    unchecked_set.discard(proxy)
+    working_set.discard(proxy)
+    broken_set.add(proxy)
+
+def get(url, proxy):
+    try:
+        # Send proxy requests to the final URL
+        response = requests.get(url, proxies={'http': f"http://{proxy}"}, timeout=10)
+        if response.status_code in VALID_STATUSES:
+            print(response.status_code, response.text)
+            set_working(proxy)
+        else:
+            set_not_working(proxy)
+    except Exception as e:
+        print("Exception: ", type(e))
+        set_not_working(proxy)
+    f = open("tested_proxies", "w")
+    for i in working_set:
+        f.write(i + "\n")
+
+def check_proxies():
+    # proxy_from_list = proxies_list.pop()
+    for proxy_from_list in proxies_list:
+        get(url="http://ident.me/", proxy=proxy_from_list)
+
+
+check_proxies()
