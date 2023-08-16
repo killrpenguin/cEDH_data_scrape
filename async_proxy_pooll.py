1d82760c7387ad6b209035a35099a8a32cd1c603
killrpenguin <dmcfarland8031@gmail.com>
2023-08-15 19:59:45 -0600

    cleaning code, renaming variables, little things.

--- async_proxy_pooll.py
+++ async_proxy_pooll.py
@@ -2,39 +2,33 @@ import asyncio
 import aiohttp
 
 
-working_set, broken_set = set(), set()
+working_set = set()
 VALID_STATUSES = [200, 301, 302, 307, 404]
 
 
 def set_working(proxy):
     working_set.add(proxy)
-    broken_set.discard(proxy)
-
-
-def set_not_working(proxy):
-    working_set.discard(proxy)
-    broken_set.add(proxy)
 
 
 async def task_coroutine(session, proxy):
     try:
         async with session.get("http://ident.me/", proxy=proxy, ssl=False, timeout=600) as resp:
+            await resp.text()
             if resp.status in VALID_STATUSES:
                 print('Status Code: ' + str(resp.status))
                 set_working(proxy)
-            await resp.text()
     except Exception as e:
         print("Exception: ", e)
 
 
 # custom coroutine
 async def main_proxy_pool() -> list:
-    proxies_list = open("MasterProxyList", "r").read().strip().split("\n")
-    tcp_connection = aiohttp.TCPConnector(limit=500)
+    proxy_list = open("MasterProxyList", "r").read().strip().split("\n")
+    tcp_connection = aiohttp.TCPConnector(limit=250)
     header = {"Authorization": "Basic bG9naW46cGFzcw=="}
     async with aiohttp.ClientSession(connector=tcp_connection, headers=header, trust_env=True) as session:
         try:
-            tasks = [asyncio.create_task(task_coroutine(session, i)) for i in proxies_list]
+            tasks = [asyncio.create_task(task_coroutine(session, i)) for i in proxy_list]
             for task in tasks:
                 await task
         except Exception as e:
@@ -45,4 +39,4 @@ async def main_proxy_pool() -> list:
     return lst
 
 
-asyncio.run(main_proxy_pool())
+# asyncio.run(main_proxy_pool())
