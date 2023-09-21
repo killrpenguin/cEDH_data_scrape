a26f8f14c1ee9a04cf0403b45fed9b756189151c
killrpenguin <dmcfarland8031@gmail.com>
2023-09-21 13:04:43 -0600

    First scrape of viable data completed. 402 Mana Crypts out of 404 decks.

--- scratch2.py
+++ scratch2.py
@@ -3,22 +3,38 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import EdgeOptions
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from dataclasses import dataclass, field
 
 
-view_text = "/html/body/div[5]/div/div/form/div[2]/div[2]/label[1]/div/input"
-proxy = "http://103.77.60.14:80"
-address = 'https://www.moxfield.com/decks/HYT9YcG0bEK1UMvmEEXtTA'
-pause = 5
+xpath = '//*[@id="proxytable"]/tr'
+page = "https://proxyscrape.com/free-proxy-list-f"
 edge_options = EdgeOptions()
 edge_options.use_chromium = True
-# edge_options.add_argument('headless')
+edge_options.add_argument('headless')
 edge_options.add_argument('disable-gpu')
-edge_options.add_argument("--proxy_server=%s" % proxy)
-driver = selenium.webdriver.Edge(options=edge_options)
-driver.get(address)
-driver.implicitly_wait(pause)
-button = driver.find_element(By.XPATH, "/html/body/div[1]/main/div[8]/div[1]/div[2]/form/a[1]")
-button.click()
-"""radio1 = driver.find_element(By.XPATH, view_text)
-radio1.click()"""
+
+proxy_objects = []
+@dataclass(kw_only=True)
+class Proxies:
+    address: str = field(default=str)
+    protocol: str = field(default=str)
+    latency: str = field(default=str)
+
+
+with selenium.webdriver.Edge(options=edge_options) as driver:
+    wait = WebDriverWait(driver, 30)
+    driver.get(page)
+    proxy_table = wait.until(ec.presence_of_all_elements_located((By.XPATH, xpath)))
+    proxy_table = [proxy.text for proxy in proxy_table]
+    for i in proxy_table:
+        address = re.search("^\d.+?\s\d.+?\s", i)
+        protocol = re.search("H.+?\s|So.+?\d\s", i)
+        latency = re.search("\d{2,4}ms", i)
+        proxy_detail = Proxies(address=address.group().replace(" ", ":", 1).strip(),
+                               protocol=protocol.group().strip(),
+                               latency=latency.group().strip()
+                               )
+
+    print(len(driver.page_source))
